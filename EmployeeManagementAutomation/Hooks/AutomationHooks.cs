80fc08fbef76616b4f3191669f193dc75cdd11a1
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-15 17:21:56 +0530

    Added Hooks Class

---
+++ EmployeeManagementAutomation/Hooks/AutomationHooks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementAutomation.Hooks
+{
+    [Binding]
+    public class AutomationHooks
+    {
+        [BeforeScenario]
+        public void RunBeforeScenario()
+        {
+            //browser launch and IPage object
+        }
+
+        [AfterScenario]
+        public void RunAfterScenario()
+        {
+            //close the browser
+        }
+    }
+}
