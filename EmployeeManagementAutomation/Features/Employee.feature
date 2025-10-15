3eca3ee446fac6d58ec2f9b59c96a9601ee74d2c
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-15 15:57:28 +0530

    Added Employee Feature

---
+++ EmployeeManagementAutomation/Features/Employee.feature
@@ -0,0 +1,14 @@
+﻿Feature: Employee
+In order to manage the employee records
+As an user
+I would like to add, edit, delete employee records
+
+
+Scenario: AddValidEmployeeTest
+	Given User have browser with Orange HRM application
+	When User enter username as "Admin"
+	And User enter password as "admin123"
+	And User click on login
+	And User click on PIM menu
+	# click pim-->add employee --> add fname, mname,lastname and save 
+	
--- EmployeeManagementAutomation/StepDefinitions/LoginStepDefinitions.cs
+++ EmployeeManagementAutomation/StepDefinitions/LoginStepDefinitions.cs
@@ -6,29 +6,37 @@ namespace EmployeeManagementAutomation.StepDefinitions
     [Binding]
     public class LoginStepDefinitions
     {
+
+
+        [Given("User have browser with Orange HRM application")]
         [Given("I have browser with Orange HRM application")]
         public void GivenIHaveBrowserWithOrangeHRMApplication()
         {
             Console.WriteLine("given");
         }
 
+        [When("User enter username as {string}")]
         [When("I enter username as {string}")]
         public void WhenIEnterUsernameAs(string username)
         {
             Console.WriteLine("when"+username);
         }
 
+        [When("User enter password as {string}")]
         [When("I enter password as {string}")]
-        public void WhenIEnterPasswordAs(string password)
+        public void WhenIEnterPasswordAs(string password="Welcome123")
         {
             Console.WriteLine(password);
         }
 
+
+        [When("User click on login")]
         [When("I click on login")]
         public void WhenIClickOnLogin()
         {
             Console.WriteLine("login");
         }
+
 
         [Then("I should access to portal with header as {string}")]
         public void ThenIShouldAccessToPortalWithHeaderAs(string expectedValue)
